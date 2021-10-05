cc92f1ecb3d4c679acf308d0efcb7f4eec4277b5
Rababalsurmi <86403067+Rababalsurmi@users.noreply.github.com>
2021-10-05 15:49:27 +0200

    OperationTests

--- Calculator.Tests/UnitTest1.cs
+++ Calculator.Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Xunit;
 using CalculatorLibrary;
 
@@ -13,18 +13,76 @@ namespace Calculator.Tests
         {
             //arrange
 
-            //string op = "a";
             double num1 = 5;
             double num2 = 3;
             double expected = 8;
 
-            //CalculatorLibrary.Calculator testcalculator = null;
-            //testcalculator = new CalculatorLibrary.Calculator(num1, num2, op);
             //act
             var actual = CalculatorLibrary.Calculator.Add(num1, num2);
 
             //Assert
             Assert.Equal(expected, actual, 0);
         }
+
+        [Fact]
+        public void Substract()
+        {
+            //arrange
+            double x1 = 10;
+            double x2 = 8;
+            double expected = 2;
+
+            //act
+            var actual = CalculatorLibrary.Calculator.Subtract(x1, x2);
+
+            //assert
+            Assert.Equal(expected, actual, 0);
+        }
+
+        [Theory(DisplayName = "Maths- Divided with parameters")]
+        [InlineData(40, 8, 5)]
+        public void Divide(double value1, double value2, double value3)
+        {
+            //arrange
+            double x1 = value1;
+            double x2 = value2;
+            double expected = value3;
+
+            //act
+            var actual = CalculatorLibrary.Calculator.Divide(x1, x2);
+
+            //assert
+            Assert.Equal(expected, actual, 0);
+        }
+
+        [Fact(Skip = "Do not run now")]
+        public void Multiply()
+        {
+            //arrange
+            double x1 = 5;
+            double x2 = 8;
+            double expected = 40;
+
+            //act
+            var actual = CalculatorLibrary.Calculator.Multiply(x1, x2);
+
+            //assert
+            Assert.Equal(expected, actual, 0);
+        }
+
+        [Fact(DisplayName = "Maths - Divide by Zero Exception")]
+        public void DivideByZeroException()
+        {
+            //arrange
+            double a = 100;
+            double b = 0;
+
+            //act
+            Action act = () => CalculatorLibrary.Calculator.Divide(a, b);
+
+            //assert
+            Assert.Throws<DivideByZeroException>(act);
+        }
     }
 }
+
